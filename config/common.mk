d1637a66091ff9fba23bda4a1d4774fd17f9545f
Shaun <shaunsibu@gmail.com>
2016-12-16 10:52:50 -0700

    Building with OMS so identify with build name
    Change-Id: Ibf10b9b075889cb3b2a420bcfcbee55c0393e46b

--- config/common.mk
+++ config/common.mk
@@ -301,7 +301,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION = 5.8.0
-    CM_VERSION := ResurrectionRemix-N-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
+    CM_VERSION := ResurrectionRemix-N-OMS-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.rr.version=$(CM_VERSION) \
