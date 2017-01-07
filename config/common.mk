0c199637c80ac469a3faef1ef1573c7ccd1e80a1
westcripp <korukaltan@gmail.com>
2017-01-07 17:46:12 +0300

    Fix default root value

--- config/common.mk
+++ config/common.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=1
+    persist.sys.root_access=2
 
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
