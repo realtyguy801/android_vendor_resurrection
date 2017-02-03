1efd7c82cd5743f1c12b88f026c7fe4327252915
Shaun <shaunsibu@gmail.com>
2017-02-02 22:57:52 -0700

    Revert "Remove prebuilt DeskClock and build from source"
    This reverts commit c6073f7a69e6224ba08793d01274424f43818aef.

--- config/common.mk
+++ config/common.mk
@@ -91,6 +91,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/Wallpaper/Wallpaper.apk:system/app/Wallpaper/Wallpaper.apk
 
+# DeskClock
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/DeskClock/DeskClock.apk:system/app/DeskClock/DeskClock.apk
+
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils
@@ -184,7 +188,6 @@ PRODUCT_PACKAGES += \
     Trebuchet \
     MusicFX \
     CMFileManager \
-    DeskClock \
     Eleven \
     LockClock \
     CMSettingsProvider \
